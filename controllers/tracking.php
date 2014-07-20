79769211d77819479c6934c6c8530561689bbbf0
desktop7 <sombra2eternity@gmail.com>
2014-07-20 06:17:12 +0200

    tracking insights

--- controllers/tracking.php
+++ controllers/tracking.php
@@ -5,11 +5,23 @@
 
 	function tracking_insights(){
 		include_once('inc.track.php');
+		include_once('inc.mongo.php');
 		$TEMPLATE = &$GLOBALS['TEMPLATE'];
 
+		$db = mongo_get();
+		$collection = $db->selectCollection('tracebat',$_SERVER['SERVER_NAME']);
+		$collection->ensureIndex(array('trackingIP'=>1));
+		$collection->ensureIndex(array('trackingUserAgent'=>1));
+		$collection->ensureIndex(array('trackingURL'=>1));
+		$collection->ensureIndex(array('trackingMS'=>1));
+		$collection->ensureIndex(array('trackingDate'=>1));
+		$collection->ensureIndex(array('trackingTime'=>1));
+		$collection->ensureIndex(array('trackingHour'=>1));
+
 		$date = date('Y-m-d');
-		$rows = tracking_getWhere('(trackingDate = \''.$date.'\')',array('order'=>'trackingMS DESC','limit'=>40,'db.file'=>$GLOBALS['api']['track']['db.tmp']));
-		$TEMPLATE['html.track.table.insight'] = '<table><tbody>';
+		$rows = $collection->find(array('trackingDate'=>$date))->sort(array('trackingMS'=>-1))->limit(40);
+
+		$TEMPLATE['html.track.table.insight'] = '<table style="width:100%;"><tbody>';
 		foreach($rows as $row){
 			$TEMPLATE['html.track.table.insight'] .= '<tr><td>'.$row['trackingURL'].'</td><td>'.$row['trackingMS'].'</td></tr>';
 		}
